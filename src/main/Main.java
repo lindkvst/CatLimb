36cebc6540deb7ffbcffed16023ec43a277c6d7b
lindkvst <lindkviste@gmail.com>
2025-11-05 12:51:10 +0100

    main.java + CatFlap

---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Main {
+    static void main() {
+        runCatFlap();
+    }
+
+    static void runCatFlap() {
+        System.out.println("Her kører vi main");
+    }
+}
