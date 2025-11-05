72deaf367a17f88192dc204e31b18434870152f6
lindkvst <lindkviste@gmail.com>
2025-11-05 12:06:51 +0100

    CatFlapTest

--- test/main/CatFlapTest.java
+++ test/main/CatFlapTest.java
@@ -7,13 +7,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class CatFlapTest {
 
     @org.junit.jupiter.api.Test
-    void testAddCat() {
-/*
-        ArrayList<Cat> catList = new ArrayList<>();
+    void testAddCatMax() {
 
-        catList.addCat("")
 
-        assertEquals()
-*/
+        //ArrayList<Cat> catList = new ArrayList<>();
+        CatFlap catFlap = new CatFlap();
+
+        catFlap.addCat(new Cat("Cat1"));
+        catFlap.addCat(new Cat("Cat2"));
+        catFlap.addCat(new Cat("Cat3"));
+        catFlap.addCat(new Cat("Cat4"));
+        catFlap.addCat(new Cat("Cat5"));
+
+
+
     }
 }
