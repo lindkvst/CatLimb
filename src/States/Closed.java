325e7423cf35eea5e9d9339cbf72fee14160ae72
Bishobos <mps.kajkage@gmail.com>
2025-11-05 10:52:41 +0100

    Closed first draft

--- src/States/Closed.java
+++ src/States/Closed.java
@@ -1,4 +1,18 @@
 package States;
 
-public class Closed {
+import main.Cat;
+
+public class Closed implements State{
+    private final CatFlap catFlap;
+    public Closed(CatFlap flap){this.catFlap = flap;}
+
+    @Override
+    public boolean enter(Cat cat){
+        return false;
+    }
+
+    @Override
+    public boolean exit(Cat cat){
+        return false;
+    }
 }
