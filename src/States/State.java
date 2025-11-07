042948da0f393923f973ad6a684abf8871b20ca1
Bishobos <mps.kajkage@gmail.com>
2025-11-07 15:10:48 +0100

    removed some redundant code

--- src/States/State.java
+++ src/States/State.java
@@ -14,13 +14,13 @@ public interface State {
      * @param cat The entering cat of type {@link Cat}
      * @return boolean for whether the cat is allowed entry
      */
-    public boolean enter(Cat cat);
+    boolean enter(Cat cat);
 
     /**
      * exit is the method dictating the logic for exiting {@link Cat}s
      * @param cat The exiting cat of type {@link Cat}
      * @return boolean for whether the cat is allowed to exit
      */
-    public boolean exit(Cat cat);
+    boolean exit(Cat cat);
 
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,5 @@
 package main;
 
-import States.Closed;
-
 public class Main {
     static void main() {
         runCatFlap();
